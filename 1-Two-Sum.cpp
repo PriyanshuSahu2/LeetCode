059d66b8605e8bcb4897d1e6bd9645184ccb879e
Priyanshu <55783652+PriyanshuSahu2@users.noreply.github.com>
2024-09-13 22:32:04 +0530

    [1] [Time Beats: 68.00%] [Memory Beats: 15.80%] - LeetPush

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -2,13 +2,23 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
-            }
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+           int t = target - nums[i];
+           if(mp.find(t) != mp.end()){
+            return {mp[t],i};
+           } 
+
+           mp[nums[i]] = i;
+            
+            
         }
-        return {}; // No solution found
+        return {}; 
     }
 };
+//[3,2,4]
+/* 
+3 0
+2 1
+
+*/
