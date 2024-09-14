0cc5128d00b374f393f67685bfa3a4682b7f1af0
Priyanshu <55783652+PriyanshuSahu2@users.noreply.github.com>
2024-09-14 19:25:08 +0530

    [169] [Time Beats: 77.09%] [Memory Beats: 36.13%] - LeetPush

--- 169-Majority-Element.cpp
+++ 169-Majority-Element.cpp
@@ -6,17 +6,7 @@ public:
         int curr = nums[0];
         int lengthCurr = 0;
 
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]==curr){
-                lengthCurr++;
-                if(lengthCurr >= nums.size()/2 + (nums.size()%2)){
-                    return nums[i];
-                }
-            }else{
-                curr = nums[i];
-                lengthCurr = 1;
-            }
-        }
-        return 0;
+
+        return nums[nums.size()/2];
     }
 };
